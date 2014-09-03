9441ed8d2b99523f1407121d2468d573f868c7b9
nguyenhieptn <nguyenhieptn@yahoo.com>
2014-09-03 18:00:42 +0700

    up

--- app/lang/vn/gen.php
+++ app/lang/vn/gen.php
@@ -4,5 +4,9 @@ return array(
 	'buu dien thai nguyen' => 'Bưu điện tỉnh Thái Nguyên',
 	'THAI NGUYEN POST'     => 'test POST',
     'unit' => "Tài Khoản",
-    'title' =>"Bưu điện tỉnh Thái Nguyên"
+    'title' =>"Bưu điện tỉnh Thái Nguyên",
+    'unit name' =>"Đơn vị",
+    'tools' =>"Tác vụ",
+    'unit list'=>'Danh sách các đơn vị hiện tại',
+    'add'=>'Thêm mới '
 );
--- app/views/units/index.blade.php
+++ app/views/units/index.blade.php
@@ -1,28 +1,46 @@
 @extends('layouts.layout')
 @section('content')
 <div class="row">
-
     <div class="span12">
-
         <div class="widget">
 
-            <div class="widget-header">
-                <i class="icon-th-large"></i>
-                <h3>Choose Your Plan</h3>
-            </div> <!-- /widget-header -->
-
-            <div class="widget-content">
-
-                Dashboard here
-
-            </div> <!-- /widget-content -->
+            <div class="widget widget-table action-table">
+                <div class="widget-header"> <i class="icon-th-list"></i>
+                    <h3>{{ trans("gen.unit list")}}</h3>
+                    <span class="pull-right"><a class="btn btn-small btn-success" href="javascript:;">{{ trans("gen.add")}}</a></span>
+                </div>
+                <!-- /widget-header -->
+                <div class="widget-content">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th width="30px">ID</th>
+                            <th>{{ trans("gen.unit name")}}</th>
+                            <th class="td-actions" width="80px"> {{ trans("gen.tools")}} </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($units as $u)
+                        <tr>
+                            <td>{{ $u->id }}</td>
+                            <td>{{ $u->name }}</td>
+                            <td class="td-actions">
+                                <a class="btn btn-small btn-success" href="javascript:;"><i class="btn-icon-only icon-edit"> </i></a>
+                                <a class="btn btn-danger btn-small" href="{{ route('units.destroy',array($u->id)) }}" data-method="delete" rel="nofollow" data-confirm="Are you sure you want to delete this?"><i class="btn-icon-only icon-remove"> </i></a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /widget-content -->
+            </div>
 
         </div> <!-- /widget -->
-
     </div> <!-- /span12 -->
-
-
 </div> <!-- /row -->
+
+
 @stop
 @section('footer')
 @parent
